589d4c1faacaa56449096cf422672f77e4fa1922
Philipp Mueller <philippm13@gmail.com>
2019-10-11 11:06:59 -0700

    fix docker example folder

--- binder/edit.sh
+++ binder/edit.sh
@@ -5,4 +5,4 @@ set -e
 CONTAINER_TAG=htcondor-python-examples
 
 docker build -t ${CONTAINER_TAG} --file binder/Dockerfile .
-docker run -it --rm -p 8888:8888 --mount type=bind,source="$(pwd)"/tutorials,target=/home/muellerp/tutorials ${CONTAINER_TAG}
+docker run -it --rm -p 8888:8888 --mount type=bind,source="$(pwd)"/Examples,target=/home/muellerp/Examples ${CONTAINER_TAG}
